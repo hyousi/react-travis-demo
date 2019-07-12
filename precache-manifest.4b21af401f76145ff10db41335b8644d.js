346f6118858109918fe900626a453125b38b76be
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-12 01:57:45 +0000

    Deploy hyousi/react-travis-demo to github.com/hyousi/react-travis-demo.git:gh-pages

---
+++ precache-manifest.4b21af401f76145ff10db41335b8644d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96b6f891992a069d890195c6cce8863",
+    "url": "/react-travis-demo/index.html"
+  },
+  {
+    "revision": "598a46cfe05995c0f871",
+    "url": "/react-travis-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-travis-demo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "598a46cfe05995c0f871",
+    "url": "/react-travis-demo/static/js/main.6237c594.chunk.js"
+  },
+  {
+    "revision": "02a4436afac71cc66761",
+    "url": "/react-travis-demo/static/js/runtime~main.03a91fc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-travis-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
